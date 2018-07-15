0228d24145800130cba8865a9d3bb124b32ade0f
S80 <stephenmanuelmiller@gmail.com>
2018-07-15 16:40:58 -0700

    Lesson5.R_COMPLETE

--- Lesson5.R
+++ Lesson5.R
@@ -62,3 +62,52 @@ with(pf, cor.test(age, friend_count, method = 'pearson'))
 with(subset(pf, age <= 70), cor.test(age, friend_count))
 
 ?subset
+
+## Strong Correlations
+
+library(ggplot2)
+
+ggplot(aes(x = www_likes_received, y = likes_received), data = pf) +
+  geom_point()+
+  xlim(0, quantile(pf$www_likes_received, 0.95)) +
+  ylim(0, quantile(pf$likes_received, 0.95))
+
+with(pf, cor.test(www_likes_received, likes_received))
+
+## More Caution with Correlations
+
+install.packages('alr3')
+
+library(alr3)
+data("Mitchell")
+summary("Mitchell")
+head(Mitchell)
+?Mitchell
+
+ggplot(data = Mitchell, aes(x = Month, y = Temp)) +
+  geom_point()
+
+cor.test(Mitchell$Month, Mitchell$Temp)
+
+ggplot(data = Mitchell, aes(x = Month, y = Temp)) +
+  geom_point() +
+  scale_x_discrete(breaks = seq(0, 203, 12))
+
+## Create an age_with_months variable
+
+pf$age_with_months <- pf$age + (1 - pf$dob_month / 12)
+
+## Age with Months Means
+
+library(dplyr)
+
+age_with_months_groups <- group_by(pf, age_with_months)
+PF.FC_by_age_months <- summarise(age_with_months_groups,
+                                 friend_count_mean = mean(friend_count),
+                                 friend_count_median = median(friend_count),
+                                 n = n())
+PF.FC_by_age_months <- arrange(PF.FC_by_age_months, age_with_months)
+head(PF.FC_by_age_months)
+
+ggplot(data = subset(PF.FC_by_age_months, age_with_months < 71), aes(x = age_with_months, y = friend_count_mean)) +
+  geom_line()
